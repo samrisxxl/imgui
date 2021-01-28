0feac29e4f602168d0c1a5e10fa61628d13bbad7
Samuel Rising <samuelrising96@gmail.com>
2021-01-28 23:21:40 +0100

    Updated includes and added imgui_stdlib.cpp

--- premake5.lua
+++ premake5.lua
@@ -5,19 +5,20 @@ project "IMGUI"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+    -- Some headers are included directly since they are placed in the root folder of ImGui
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
-		"imgui_internal.h",
 		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "misc/cpp/imgui_stdlib.cpp"
 	}
+
+    includedirs
+    {
+        "../imgui"
+    }
 
 	filter "system:windows"
 		systemversion "latest"
